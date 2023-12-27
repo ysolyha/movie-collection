eb9f18fde10214729a1e22fa9b43f0950f220721
ysolyha <yurii.solyha@code-seek.com>
2023-12-27 13:32:52 +0200

    init commit

---
+++ src/main/java/com/codeseek/moviecollection/MovieCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.codeseek.moviecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCollectionApplication.class, args);
+	}
+
+}
